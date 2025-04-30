cdf7b7f854a6ffceaca17ad720e2a7a300503b20
Kayra <kayra.gemalmaz@canonical.com>
2025-04-30 19:50:42 +0300

    fix: integration tests are sequential

--- tests/integration/test_secrets.py
+++ tests/integration/test_secrets.py
@@ -16,9 +16,9 @@ def test_add_secret(juju: jubilant.Juju):
 
 
 def test_update_secret(juju: jubilant.Juju):
-    uri = juju.secret('sec1', {'username': 'usr', 'password': 'hunter2'}, info='A description.')
+    uri = juju.secret('sec2', {'username': 'usr', 'password': 'hunter2'}, info='A description.')
     uri2 = juju.secret(
-        'sec1', {'username': 'usr', 'password': 'hunter3'}, info='A new description.'
+        'sec2', {'username': 'usr', 'password': 'hunter3'}, info='A new description.', update=True
     )
 
     assert uri == uri2
@@ -36,13 +36,6 @@ def test_get_all_secrets(juju: jubilant.Juju):
         {'label': 'sec1', 'username': 'usr', 'password': 'hunter2', 'info': 'A description'},
         {'label': 'sec2', 'username': 'usr2', 'password': 'hunter3', 'info': 'A new description'},
     ]
-    for arg in args:
-        uri = juju.secret(
-            arg['label'],
-            {'username': arg['username'], 'password': arg['password']},
-            info=arg['info'],
-        )
-        uris.append(uri)
     secrets = juju.secret()
 
     assert isinstance(secrets, list)
@@ -57,13 +50,8 @@ def test_get_all_secrets(juju: jubilant.Juju):
 
 
 def test_get_secret(juju: jubilant.Juju):
-    uri = juju.secret('sec1', {'username': 'usr', 'password': 'hunter2'}, info='A description.')
-
     secret = juju.secret(label='sec1')
-    secret2 = juju.secret(uri=uri)
 
-    assert secret == secret2
-    assert secret.uri == uri
     assert secret.revision == 1
     assert secret.owner == '<model>'
     assert secret.name == 'sec1'
@@ -71,3 +59,6 @@ def test_get_secret(juju: jubilant.Juju):
     assert secret.created.year == datetime.datetime.now().year
     assert secret.created == secret.updated
     assert secret.content == {'username': 'usr', 'password': 'hunter2'}
+
+    secret2 = juju.secret(uri=secret.uri)
+    assert secret == secret2
